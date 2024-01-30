388ad798c63ea7a7150306f790a7c152391246d2
Thomas Junk <thomas.junk@intevation.de>
2024-01-30 11:38:26 +0100

    display data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script>
   import Webview from "$lib/CSAFWebview/Webview.svelte";
+  import { convertToDocModel } from "$lib/CSAFWebview/docmodel/docmodel";
+  import { appStore } from "$lib/store";
+  import { doc } from "$lib/testdata";
+  const docModel = convertToDocModel(doc);
+  appStore.setDocument(docModel);
 </script>
 
 <Webview></Webview>
